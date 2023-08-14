59b8b080025f6135a74243d9bdb476a9b86f21ab
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-13 22:53:57 -0500

    freeze opcodes constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // Defined here <https://www.rfc-editor.org/rfc/rfc6455#page-29>
-export const OPCODES = {
+export const OPCODES = Object.freeze({
   continuation: 0x00,
   text: 0x01,
   binary: 0x02,
@@ -8,6 +8,6 @@ export const OPCODES = {
   ping: 0x09,
   pong: 0x0A,
   // B...F are reserved
-} as const;
+});
 
 export const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
