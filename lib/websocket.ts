e95882000f5382f49f5e80b1ba57e13a7abb68a3
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-13 20:52:12 -0500

    fix: fix opcode 4 bits extraction

--- lib/websocket.ts
+++ lib/websocket.ts
@@ -29,8 +29,7 @@ export class WebSocketConnection extends EventEmitter {
     // Process first byte
     // 0:[FIN] 1..3:[reserved] 4..7:[opcode]
     const firstByte = data.readUint8(0);
-    // const fin = (firstByte & 0b10000000) >> 7; // if set, this is the final fragment
-    const opcode = firstByte & 0b0001111;
+    const opcode = firstByte & 0b00001111;
 
     if (opcode === OPCODES.close) {
       this.socket.end();
@@ -52,9 +51,32 @@ export class WebSocketConnection extends EventEmitter {
     const len = secondByte & 0b01111111;
     // eslint-disable-next-line no-nested-ternary
     const effectiveLen = len <= 125 ? len : (
-      len === 126 ? data.readUInt16BE(2) : data.readUInt32BE(2)
+      len === 126 ? data.readUint16BE(2) : data.readUint32BE(2)
     );
 
     console.log({ firstByte, opcode: entry ?? 'invalid' });
   }
 }
+
+/**
+ * Wire format defined in <https://www.rfc-editor.org/rfc/rfc6455#section-5.2>
+ *
+ *   0                   1                   2                   3
+ *   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *  +-+-+-+-+-------+-+-------------+-------------------------------+
+ *  |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+ *  |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+ *  |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+ *  | |1|2|3|       |K|             |                               |
+ *  +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+ *  |     Extended payload length continued, if payload len == 127  |
+ *  + - - - - - - - - - - - - - - - +-------------------------------+
+ *  |                               |Masking-key, if MASK set to 1  |
+ *  +-------------------------------+-------------------------------+
+ *  | Masking-key (continued)       |          Payload Data         |
+ *  +-------------------------------- - - - - - - - - - - - - - - - +
+ *  :                     Payload Data continued ...                :
+ *  + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+ *  |                     Payload Data continued ...                |
+ *  +---------------------------------------------------------------+
+ */
